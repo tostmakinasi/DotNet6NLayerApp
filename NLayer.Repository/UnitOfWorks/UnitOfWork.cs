2701dd0b009eb04d165895438aa5f425a59aa0cd
Kubilay Cem <kubilaycem@yandex.com>
2022-02-15 20:38:17 +0300

    Generic Repository and Unit Of Work Implementation

---
+++ NLayer.Repository/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using NLayer.Core.UnitOfWorks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository.UnitOfWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+        public void CommitChanges()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task CommitChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
