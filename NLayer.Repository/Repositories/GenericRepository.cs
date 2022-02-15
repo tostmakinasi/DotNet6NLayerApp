9d5a4fe86d3e9850c55d88222032098000c7ef76
Kubilay Cem <kubilaycem@yandex.com>
2022-02-16 01:26:26 +0300

    Implementation Service, Configured Core Layer

--- NLayer.Repository/Repositories/GenericRepository.cs
+++ NLayer.Repository/Repositories/GenericRepository.cs
@@ -33,7 +33,7 @@ namespace NLayer.Repository.Repositories
             return await _dbSet.AnyAsync(expression);
         }
 
-        public IQueryable<T> GetAll(Expression<Func<T, bool>> expression)
+        public IQueryable<T> GetAll()
         {
             return _dbSet.AsNoTracking().AsQueryable();
         }
