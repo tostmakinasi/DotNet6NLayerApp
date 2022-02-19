ab24bde38ae095e709744669ca8b3b83e96590e2
Kubilay Cem <kubilaycem@yandex.com>
2022-02-19 13:48:52 +0300

    Repository : Product and Category Respository Implementation

---
+++ NLayer.Repository/Repositories/ProductRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayer.Core;
+using NLayer.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository.Repositories
+{
+    public class ProductRepository : GenericRepository<Product>, IProductRepository
+    {
+        public ProductRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public async Task<List<Product>> GetAllProductsWithCategory()
+        {
+            return await _context.Products.Include(x=> x.Category).ToListAsync();
+        }
+
+        //public async Task<List<Product>> GetProductsWithCategoryByCategoryId(int categoryId)
+        //{
+        //    return await _context.Products.Where(x=> x.CategoryId == categoryId).Include(x=> x.Category).ToListAsync();
+        //}
+
+        public async Task<Product> GetProductsWithCategoryById(int id)
+        {
+            return await _context.Products.Include(x => x.Category).FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+    }
+}
