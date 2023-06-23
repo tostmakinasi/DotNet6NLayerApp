d9f75c715c87c8c1f7862ce8d1e31e9fe30b8afd
Kubilay Cem KARAHAN <kubilaycem@yandex.com>
2023-06-24 02:10:01 +0300

    API -- Configured Caching Module

--- NLayer.API/Program.cs
+++ NLayer.API/Program.cs
@@ -31,8 +31,13 @@ builder.Services.Configure<ApiBehaviorOptions>(options =>
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddMemoryCache();
+
+
 builder.Services.AddScoped(typeof(NotFoundFilter<>));
 builder.Services.AddAutoMapper(typeof(MapProfile));
+
+
 
 builder.Services.AddDbContext<AppDbContext>(x =>
 {
